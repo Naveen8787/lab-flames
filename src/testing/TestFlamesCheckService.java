d3dd2bc42038ca5ce0644a59f59f3f17727b5dc0
Naveen8787 <61699352+Naveen8787@users.noreply.github.com>
2020-03-24 19:02:00 +0530

    updated Test-cases

--- src/testing/TestFlamesCheckService.java
+++ src/testing/TestFlamesCheckService.java
@@ -1,7 +1,6 @@
 package testing;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import service.FlamesCheckService;
@@ -9,44 +8,30 @@ import service.FlamesCheckService;
 public class TestFlamesCheckService {
 	FlamesCheckService fcs = new FlamesCheckService();
 	@Test
-	public void testFindFlamesMethod() {
-		
-		String name1 = "nani";
-		String name2 = "ammu";
-		char temp = 'm';
+ 	public void testFindFlamesMethod() {
+ 		String name1 = "Alex";
+ 		String name2 = "Joylin";
+ 		char temp = 'a';
+ 		assertEquals(temp,fcs.findFlames(name1, name2));
+ 		name1 = "Steffe";
+ 		name2 = "Bobby";
+ 		temp = 'm';
 		assertEquals(temp,fcs.findFlames(name1, name2));
-		
-		name1 = "Naveen";
-		name2 = "Divya";
-		temp = 'e';		assertEquals(temp,fcs.findFlames(name1, name2));
-		
-		name1 = "Varsha";
-		name2 = "Naveen";
-		temp = 'a';
-		assertEquals(temp,fcs.findFlames(name1, name2));
-		
-		name1 = "Naveen";
-		name2 = "Shiri";
-	     temp = 'm';
-		assertEquals(temp,fcs.findFlames(name1, name2));
-		
-		name1 = "Sai prasad";
-		name2 = "Laddu";
-		temp = 'l';
-		assertEquals(temp,fcs.findFlames(name1, name2));
-		
-		name1 = "acxdgjikz";
-		name2 = "b";
-		temp = 'l';
-		assertEquals(temp,fcs.findFlames(name1, name2));
-		
-		try {
-			fcs.findFlames(null, null);
-			fcs.findFlames("AAA", null);
-			fcs.findFlames(null, "BBB");
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+ 		name1 = "John";
+ 		name2 = "Jully";
+ 		temp = 'e';
+ 		assertEquals(temp,fcs.findFlames(name1, name2));
+ 		name1 = "George";
+ 		name2 = "Neythiri";
+ 		temp = 'l';
+	    assertEquals(temp,fcs.findFlames(name1, name2));
+ 		try {
+ 			fcs.findFlames(null, null);
+ 			fcs.findFlames("AAA", null);
+ 			fcs.findFlames(null, "BBB");
+ 		}
+ 		catch(Exception e) {
+ 			e.printStackTrace();
+ 		}
+ 	}
 }

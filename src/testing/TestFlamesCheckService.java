ff130a805b46c8b89825e84bd560fa8a4d429bcd
Naveen8787 <61699352+Naveen8787@users.noreply.github.com>
2020-03-24 16:08:52 +0530

    updated java Files

--- src/testing/TestFlamesCheckService.java
+++ src/testing/TestFlamesCheckService.java
@@ -1,6 +1,7 @@
 package testing;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import service.FlamesCheckService;
@@ -8,30 +9,44 @@ import service.FlamesCheckService;
 public class TestFlamesCheckService {
 	FlamesCheckService fcs = new FlamesCheckService();
 	@Test
-// 	public void testFindFlamesMethod() {
-// 		String name1 = "Alex";
-// 		String name2 = "Joylin";
-// 		char temp = 'a';
-// 		assertEquals(temp,fcs.findFlames(name1, name2));
-// 		name1 = "Steffe";
-// 		name2 = "Bobby";
-// 		temp = 'm';
-// 		assertEquals(temp,fcs.findFlames(name1, name2));
-// 		name1 = "John";
-// 		name2 = "Jully";
-// 		temp = 'e';
-// 		assertEquals(temp,fcs.findFlames(name1, name2));
-// 		name1 = "George";
-// 		name2 = "Neythiri";
-// 		temp = 'l';
-// 	    assertEquals(temp,fcs.findFlames(name1, name2));
-// 		try {
-// 			fcs.findFlames(null, null);
-// 			fcs.findFlames("AAA", null);
-// 			fcs.findFlames(null, "BBB");
-// 		}
-// 		catch(Exception e) {
-// 			e.printStackTrace();
-// 		}
-// 	}
+	public void testFindFlamesMethod() {
+		
+		String name1 = "nani";
+		String name2 = "ammu";
+		char temp = 'm';
+		assertEquals(temp,fcs.findFlames(name1, name2));
+		
+		name1 = "Naveen";
+		name2 = "Divya";
+		temp = 'e';		assertEquals(temp,fcs.findFlames(name1, name2));
+		
+		name1 = "Varsha";
+		name2 = "Naveen";
+		temp = 'a';
+		assertEquals(temp,fcs.findFlames(name1, name2));
+		
+		name1 = "Naveen";
+		name2 = "Shiri";
+	     temp = 'm';
+		assertEquals(temp,fcs.findFlames(name1, name2));
+		
+		name1 = "Sai prasad";
+		name2 = "Laddu";
+		temp = 'l';
+		assertEquals(temp,fcs.findFlames(name1, name2));
+		
+		name1 = "acxdgjikz";
+		name2 = "b";
+		temp = 'l';
+		assertEquals(temp,fcs.findFlames(name1, name2));
+		
+		try {
+			fcs.findFlames(null, null);
+			fcs.findFlames("AAA", null);
+			fcs.findFlames(null, "BBB");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
